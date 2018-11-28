cce23ae2dfb9819301076f6209c04bc6ff7af874
tgray017 <tgray017@gmail.com>
2018-11-28 01:50:16 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -18,8 +18,13 @@ class Game
   end
   
   def won?
-    WIN_COMBINATIONS.detect{|c|
+    if WIN_COMBINATIONS.detect{|c|
+      (board.cells[c[0]] == "X" && board.cells[c[1]] == "X" && board.cells[c[2]] == "X") || (board.cells[c[0]] == "O" && board.cells[c[1]] == "O" && board.cells[c[2]] == "O")}.nil?
+      false
+    else
+      WIN_COMBINATIONS.detect{|c|
       (board.cells[c[0]] == "X" && board.cells[c[1]] == "X" && board.cells[c[2]] == "X") || (board.cells[c[0]] == "O" && board.cells[c[1]] == "O" && board.cells[c[2]] == "O")}
+    end
   end
   
   def draw?
